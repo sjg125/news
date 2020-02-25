277c837efbccf3e917c743c00875eb8cb1196615
sjg125 <sam.gersten@gmail.com>
2020-02-25 21:47:26 +0000

    2/25/2020 - 3:47pm

--- app.rb
+++ app.rb
@@ -10,36 +10,33 @@ before { puts "Parameters: #{params}" }
 ForecastIO.api_key = "65f73d6491ab81faa4973089255625d7"
 # News API = 
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=82b67568e8644a85bd77b4656a2d2818"
-@news = HTTParty.get(url).parsed_response.to_hash
 
 get "/" do
-  # show a view that asks for the location
+    # show a view that asks for the location
     view "ask"
 end
 
 get "/news" do
-  # do everything else
+    # do everything else
 
-  # params is a hash built into Ruby
-  # "location" is the name of a key, or a property within the hash. It is drawn from the ask.erb tab, where it was the name created for whatever is input into the form
-  # lat_lng is not a hash - it's an array. 
-  # If we have # in curly brackets with ruby syntax means that there is string interpolation
-  # results is an array, recalling the first element of the array, then getting the coordinates  
+    # params is a hash built into Ruby
+    # "location" is the name of a key, or a property within the hash. It is drawn from the ask.erb tab, where it was the name created for whatever is input into the form
+    # lat_lng is not a hash - it's an array. 
+    # If we have # in curly brackets with ruby syntax means that there is string interpolation
+    # results is an array, recalling the first element of the array, then getting the coordinates  
 
-results = Geocoder.search(params["location"])
-lat_lng = results.first.coordinates
-lat = lat_lng[0]
-lng = lat_lng[1]
+    results = Geocoder.search(params["location"])
+    lat_lng = results.first.coordinates
+    lat = lat_lng[0]
+    lng = lat_lng[1]
 
     #you need @ signs if you want to display the information in the erb file and consequently the browser
 
-@forecast = ForecastIO.forecast("#{lat}","#{lng}").to_hash
-@current_temp = @forecast["currently"]["temperature"] 
-@current_weather = @forecast["currently"]["summary"]
+    @forecast = ForecastIO.forecast("#{lat}","#{lng}").to_hash
+    @current_temp = @forecast["currently"]["temperature"] 
+    @current_weather = @forecast["currently"]["summary"]
 
-# @Title = @news["title"]
-# @Description = @news["description"]
-# @Link= @news["link"]
+    @news = HTTParty.get(url).parsed_response.to_hash
 
     view "news"
 end
