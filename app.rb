7e6432eeefb3c39d7b5aafc4df03985d42bb48f4
sjg125 <sam.gersten@gmail.com>
2020-02-25 15:17:13 +0000

    2/25/2020 - amendments 9:16am

--- app.rb
+++ app.rb
@@ -14,31 +14,20 @@ news = HTTParty.get(url).parsed_response.to_hash
 
 get "/" do
   # show a view that asks for the location
+    view "ask"
 end
 
 get "/news" do
   # do everything else
 
-    results = Geocoder.search(params["q"])
-    latlong = results.first.coordinates
-    lat = "#{latlong[0]}"
-    long = "#{latlong[1]}"
+    results = Geocoder.search(params["location"])
+    lat_lng = results.first.coordinates
+    lat = "#{lat_lng[0]}"
+    lng = "#{lat_lng[1]}"
+    
     @forecast = ForecastI0.forecast("#{lat}","#{long}").to_hash
     @current_temp = forecast["currently"]["temperature"]
     @daily_forecast = for day in @forecast["daily"]["data"]
     "A high temperature of #{day["temperatureHigh"]},#{day[summary]}"
 
 end
-
-# view "ask"
-#results.first.coordinates #=> [latitude,longitude] --!>
-
-#location = results.first.city
-
-#puts "#{latitude},#{longitude}"
-
-# <form action "/">
-#     <label>location:</label>
-#     <input name="location">
-#     <button type="submit">
-# </form>
