fe5171a4cbd94b3d432ad4e706af824fc20956b0
sjg125 <sam.gersten@gmail.com>
2020-02-25 20:49:37 +0000

    2/25/2020 - 2:49pm

--- app.rb
+++ app.rb
@@ -22,26 +22,24 @@ get "/news" do
 
   # params is a hash built into Ruby
   # "location" is the name of a key, or a property within the hash. It is drawn from the ask.erb tab, where it was the name created for whatever is input into the form
-    results = Geocoder.search(params["location"])
   # lat_lng is not a hash - it's an array. 
   # If we have # in curly brackets with ruby syntax means that there is string interpolation
-  # results is an array, recalling the first element of the array, then getting the coordinates
-    lat_lng = results.first.coordinates
-    lat = lat_lng[0]
-    lng = lat_lng[1]
+  # results is an array, recalling the first element of the array, then getting the coordinates  
 
-    #need @ signs if want to display the information in the erb file and consequently the browser
-    @forecast = ForecastIO.forecast("#{lat}","#{lng}").to_hash
-    #@current_temp = @forecast["currently"]["temperature"] 
-    #@current_weather = @forecast["currently"]["data"][0]["summary"]
-    @todaysforecast = "The forecast for today is a high temperature of #{@forecast["daily"]["data"][0]["temperatureHigh"]} 
-    and #{@forecast["daily"]["data"][0]["summary"]}"
-    @tomorrowsforecast = "Tomorrow's forecast is a high temperature of #{@forecast["daily"]["data"][1]["temperatureHigh"]} 
-    and #{@forecast["daily"]["data"][1]["summary"]}"
+results = Geocoder.search(params["location"])
+lat_lng = results.first.coordinates
+lat = lat_lng[0]
+lng = lat_lng[1]
 
-    # @Title = "#{@news["title"][0]}"
-    # @Description = "#{@news["description"][0]}"
-    # @Link= "#{@news["link"][0]}"
+    #you need @ signs if you want to display the information in the erb file and consequently the browser
+
+@forecast = ForecastIO.forecast("#{lat}","#{lng}").to_hash
+@current_temp = @forecast["currently"]["temperature"] 
+@current_weather = @forecast["currently"]["summary"]
+
+# @Title = @news["title"]
+# @Description = @news["description"]
+# @Link= @news["link"]
 
     view "news"
 end
